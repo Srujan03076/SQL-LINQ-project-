b03785cf726677d83339f288b2cf9e76773a1e20
“Srujan03076” <srujanmahaja5@gmail.com>
2021-11-18 15:37:38 +0530

    [Srujan]Added 25 default values in list

--- ProductReview.cs
+++ ProductReview.cs
@@ -9,7 +9,7 @@ namespace LinqProject
         public int ProductId { get; set; }
         public int UserID { get; set; }
         public double Rating { get; set; }
-        public string Reveiew { get; set; }
+        public string Review { get; set; }
         public bool isLike { get; set; }
     }
 }
